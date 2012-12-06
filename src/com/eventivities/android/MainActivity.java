a06c6be01f42375fba48cc5df47f311b9787468b
analmar <analmar@upv.es>
2012-12-06 01:51:09 +0100

    cambiado los textos de las actividades creadas para que pongan en vez de hello ... el nombre de la actividad.

--- src/com/eventivities/android/MainActivity.java
+++ src/com/eventivities/android/MainActivity.java
@@ -8,6 +8,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	// Prueba
@@ -28,6 +30,36 @@ public class MainActivity extends Activity {
         */
     }
 
+    public void accion1(View v){
+    	TnUtil.vibrar(this);
+    	Toast.makeText(this,"Esto NO ESTA IMPLEMENTADO",Toast.LENGTH_SHORT).show();
+    }
+
+    public void accion2(View v){
+    	TnUtil.vibrar(this);
+    	/*
+    	 * AQUI FALTA poner lo de poner el parametro para que solo salgan los teatros 
+    	 *  es eso de pasar parametros a las activities
+    	 * 
+    	 * */
+    	startActivity(new Intent(MainActivity.this, TodosLocales.class));
+    	overridePendingTransition(R.anim.zoom_enter, R.anim.zoom_exit);
+    }
+    
+
+    public void accion3(View v){
+    	TnUtil.vibrar(this);
+    	startActivity(new Intent(MainActivity.this, QueTengoCerca.class));
+    	overridePendingTransition(R.anim.zoom_enter, R.anim.zoom_exit);
+    }
+
+    public void accion4(View v){
+    	TnUtil.vibrar(this);
+    	Toast.makeText(this,"Esto NO ESTA IMPLEMENTADO",Toast.LENGTH_SHORT).show();
+    }
+    
+    
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
