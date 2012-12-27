f10e0d7fd0f15b38a40d7eba90bf73da98d3fe22
rbrodyr <marcosgallach@gmail.com>
2012-12-27 12:15:09 +0100

    servicio Web obtener puntuaciones locales acabado

---
+++ Eventivities/src/com/eventivities/android/domain/ListaEventos.java
@@ -0,0 +1,18 @@
+package com.eventivities.android.domain;
+
+import java.util.List;
+
+
+public class ListaEventos {	
+
+	private List<Evento> eventos;
+		public List<Evento> getEventos() {
+		return eventos;
+	}
+		public void setEventos(List<Evento> eventos) {
+		this.eventos = eventos;
+	}
+
+}
+	
+
--- Eventivities/src/com/eventivities/android/servicioweb/Conexion.java
+++ Eventivities/src/com/eventivities/android/servicioweb/Conexion.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,14 +14,17 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.eventivities.android.domain.ListaObras;
-import com.eventivities.android.domain.ListaTeatros;
+
+import com.eventivities.android.domain.ListaEventos;
 import com.eventivities.android.excepciones.ExcepcionAplicacion;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -34,7 +38,9 @@ import com.google.gson.GsonBuilder;
 */
 public class Conexion {
 	
-	private final static String url="http://10.0.2.2/www/";
+	
+	private final static String url="http://www.eventivitiesadm.eshost.es/servicioweb/";
+	//private final static String url="http://10.0.2.2/www/";	
 	
 	/**
 	 * Devuelve un listado de obras de un teatro
@@ -48,20 +54,40 @@ public class Conexion {
 	* @return      la lista de obras
 	* @see         Conexion
 	*/
-	public static ListaObras obtenerObrasTeatro(String idTeatro) throws ExcepcionAplicacion
+	public static ListaEventos obtenerEventosLocal(String idLocal) throws ExcepcionAplicacion
 	{
 		List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-		pairs.add(new BasicNameValuePair("idteatro", idTeatro));	
+		pairs.add(new BasicNameValuePair("idLocal", idLocal));	
 		JSONObject json;
-		ListaObras respuesta = null;
+		ListaEventos respuesta = null;
 		try {
-			json = obtenerJsonDelServicio(pairs,"service.obtenerobrasciudad.php");
+			json = obtenerJsonDelServicio(pairs,"service.obtenereventoslocal.php");
+			int exito=1;
 			if(json!=null)
 			{			
-				GsonBuilder gsonBuilder = new GsonBuilder();
-				Gson gson = gsonBuilder.create();
-				respuesta = gson.fromJson(json.toString(), ListaObras.class);
+				if (json.has("exito"))
+				{
+					if(json.getString("exito").equalsIgnoreCase("1"))
+					{
+						GsonBuilder gsonBuilder = new GsonBuilder();
+						gsonBuilder.setDateFormat("yyyy-MM-dd");
+						Gson gson = gsonBuilder.create();				
+						respuesta = gson.fromJson(json.toString(), ListaEventos.class);
+					}
+					else
+					{
+						exito=0;
+					}
+				}
+				else
+				{
+					exito=0;
+				}
+				if (exito==0)
+					throw new ExcepcionAplicacion("El servicio web no ha respondido con exito",ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
+				
 			}	
+				
 		} catch (ClientProtocolException c)
 		{
 			throw new ExcepcionAplicacion(c.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
@@ -82,25 +108,65 @@ public class Conexion {
 		return respuesta;
 	}
 	
-	/**
-	 * Devuelve un objeto JSON de un servicio web
-	* <p>
-	* Si la búsqueda no produce ningún resultado, Json será null
-	* <p> 
-	* 
-	*
-	* @author marcos
-	* @exception En caso de problemas de conexión con el servicio Web lanzará una excepción
-	* @param  pairs pares de valores para enviar a través de Post
-	* @return      Devuelve un Objeto Json con los datos solicitados 
-	* @see         Conexion
-	*/
+	public static ListaEventos obtenerPuntuacionesEvento(String idEvento) throws ExcepcionAplicacion
+	{
+		List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+		pairs.add(new BasicNameValuePair("idEvento", idEvento));	
+		JSONObject json;
+		ListaEventos respuesta = null;
+		try {
+			json = obtenerJsonDelServicio(pairs,"service.obtenerpuntuacionesevento.php");
+			int exito=1;
+			if(json!=null)
+			{			
+				if (json.has("exito"))
+				{
+					if(json.getString("exito").equalsIgnoreCase("1"))
+					{
+						GsonBuilder gsonBuilder = new GsonBuilder();
+						gsonBuilder.setDateFormat("yyyy-MM-dd");
+						Gson gson = gsonBuilder.create();				
+						respuesta = gson.fromJson(json.toString(), ListaEventos.class);
+					}
+					else
+					{
+						exito=0;
+					}
+				}
+				else
+				{
+					exito=0;
+				}
+				if (exito==0)
+					throw new ExcepcionAplicacion("El servicio web no ha respondido con exito",ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
+				
+			}	
+				
+		} catch (ClientProtocolException c)
+		{
+			throw new ExcepcionAplicacion(c.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
+		} catch (JSONException e) {
+			e.printStackTrace();
+			throw new ExcepcionAplicacion(e.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+			throw new ExcepcionAplicacion(e.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new ExcepcionAplicacion(e.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			throw new ExcepcionAplicacion(e.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
+		}
+		return respuesta;
+	}
 	
 	private static JSONObject obtenerJsonDelServicio(List<NameValuePair> pairs, String servicio) throws ClientProtocolException, IOException, JSONException {
 		HttpClient client = new DefaultHttpClient();		
 		JSONObject json=null;		
-		
-		HttpPost request = new HttpPost(url+servicio);
+				
+		HttpPost request = new HttpPost(url+servicio);		
 		request.setHeader("Accept","application/json");	
 		request.setEntity(new UrlEncodedFormEntity(pairs));
 		HttpResponse response = client.execute(request);
@@ -122,55 +188,6 @@ public class Conexion {
 		}			
 		return json;
 	
-	}
+	}	
 	
-	/**
-	 * Devuelve un listado de teatros de una ciudad
-	* <p>
-	* Si la búsqueda no produce ningún resultado, 
-	* 
-	*
-	* @author marcos
-	* @
-	* @param  idCiudad el identificador único de la ciudad 
-	* @return      la lista de Teatros
-	* @see         Conexion
-	*/
-	
-
-	public static ListaTeatros obtenerTeatrosCiudad(String idCiudad) throws ExcepcionAplicacion
-	{
-		List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-		pairs.add(new BasicNameValuePair("idciudad", idCiudad));	
-		JSONObject json;
-		ListaTeatros respuesta = null;
-		try {
-			json = obtenerJsonDelServicio(pairs,"service.obtenerTeatrosciudad.php");
-			if(json!=null)
-			{			
-				GsonBuilder gsonBuilder = new GsonBuilder();
-				Gson gson = gsonBuilder.create();
-				respuesta = gson.fromJson(json.toString(), ListaTeatros.class);
-			}	
-		} catch (ClientProtocolException c)
-		{
-			throw new ExcepcionAplicacion(c.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
-		} catch (JSONException e) {
-			e.printStackTrace();
-			throw new ExcepcionAplicacion(e.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
-		} catch (IllegalStateException e) {
-			e.printStackTrace();
-			throw new ExcepcionAplicacion(e.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new ExcepcionAplicacion(e.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-			throw new ExcepcionAplicacion(e.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
-		}
-		return respuesta;
-	}
-	
-
 }
