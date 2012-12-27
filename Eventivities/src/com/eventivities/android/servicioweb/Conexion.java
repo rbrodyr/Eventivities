d254d02bf066517ad16e3e6b91efcbadd8855057
rbrodyr <marcosgallach@gmail.com>
2012-12-27 12:57:11 +0100

    Servicio web obtenerPuntuacionesEvento y obtenerEventosLocal probado y en funcionamiento

--- Eventivities/src/com/eventivities/android/servicioweb/Conexion.java
+++ Eventivities/src/com/eventivities/android/servicioweb/Conexion.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,17 +13,14 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
 import com.eventivities.android.domain.ListaEventos;
+import com.eventivities.android.domain.ListaPuntuaciones;
 import com.eventivities.android.excepciones.ExcepcionAplicacion;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -43,15 +39,15 @@ public class Conexion {
 	//private final static String url="http://10.0.2.2/www/";	
 	
 	/**
-	 * Devuelve un listado de obras de un teatro
+	 * Devuelve un listado de eventos de un local
 	* <p>
-	* Si la búsqueda no produce ningún resultado, 
+	* Si la búsqueda no produce ningún resultado, se devuelve una lista vacía  
 	* 
 	*
 	* @author marcos
 	* @
-	* @param  idTeatro el identificador único del teatro 
-	* @return      la lista de obras
+	* @param  idLocal el identificador único del local 
+	* @return      la lista eventos
 	* @see         Conexion
 	*/
 	public static ListaEventos obtenerEventosLocal(String idLocal) throws ExcepcionAplicacion
@@ -84,7 +80,7 @@ public class Conexion {
 					exito=0;
 				}
 				if (exito==0)
-					throw new ExcepcionAplicacion("El servicio web no ha respondido con exito",ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
+					throw new ExcepcionAplicacion("El servicio web no ha respondido con éxito",ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
 				
 			}	
 				
@@ -108,12 +104,24 @@ public class Conexion {
 		return respuesta;
 	}
 	
-	public static ListaEventos obtenerPuntuacionesEvento(String idEvento) throws ExcepcionAplicacion
+	/**
+	 * Devuelve un listado de puntuaciones de un evento
+	* <p>
+	* Si la búsqueda no produce ningún resultado, se devuelve una lista vacía  
+	* 
+	*
+	* @author marcos
+	* @
+	* @param  idEvento el identificador único del Evento 
+	* @return      la lista de puntuaciones
+	* @see         Conexion
+	*/	
+	public static ListaPuntuaciones obtenerPuntuacionesEvento(String idEvento) throws ExcepcionAplicacion
 	{
 		List<NameValuePair> pairs = new ArrayList<NameValuePair>();
 		pairs.add(new BasicNameValuePair("idEvento", idEvento));	
 		JSONObject json;
-		ListaEventos respuesta = null;
+		ListaPuntuaciones respuesta = null;
 		try {
 			json = obtenerJsonDelServicio(pairs,"service.obtenerpuntuacionesevento.php");
 			int exito=1;
@@ -123,10 +131,9 @@ public class Conexion {
 				{
 					if(json.getString("exito").equalsIgnoreCase("1"))
 					{
-						GsonBuilder gsonBuilder = new GsonBuilder();
-						gsonBuilder.setDateFormat("yyyy-MM-dd");
+						GsonBuilder gsonBuilder = new GsonBuilder();						
 						Gson gson = gsonBuilder.create();				
-						respuesta = gson.fromJson(json.toString(), ListaEventos.class);
+						respuesta = gson.fromJson(json.toString(), ListaPuntuaciones.class);
 					}
 					else
 					{
@@ -160,7 +167,8 @@ public class Conexion {
 			throw new ExcepcionAplicacion(e.getMessage(),ExcepcionAplicacion.EXCEPCION_CONEXION_SERVIDOR);
 		}
 		return respuesta;
-	}
+	}	
+
 	
 	private static JSONObject obtenerJsonDelServicio(List<NameValuePair> pairs, String servicio) throws ClientProtocolException, IOException, JSONException {
 		HttpClient client = new DefaultHttpClient();		
