bbcd0d7bc580f9d502055c351f7a335cff00676b
jorpaspr <jorpaspr@gmail.com>
2013-01-07 11:21:40 +0100

    Reajustado el nombre de la actividad Ubicacion.

--- Eventivities/src/com/eventivities/android/UbicacionActivity.java
+++ Eventivities/src/com/eventivities/android/UbicacionActivity.java
@@ -2,19 +2,29 @@ package com.eventivities.android;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 
-public class Ubicacion extends SherlockActivity {
+public class UbicacionActivity extends SherlockActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_ubicacion);
 		getSupportActionBar().setHomeButtonEnabled(true);
+		
+		TextView textView = (TextView) findViewById(R.id.textViewUbicacion);
+		textView.setText("Su ubicación actual está establecida en <b>Valencia</b>.");
+		Button buttonCambiar = (Button) findViewById(R.id.buttonCambiar);
+		buttonCambiar.setText("Cambiar ciudad...");
+		buttonCambiar.setEnabled(false);
+		Button buttonMaps = (Button) findViewById(R.id.buttonMaps);
+		buttonMaps.setText("Qué tengo cerca");
 	}
 	
 	@Override
@@ -28,11 +38,11 @@ public class Ubicacion extends SherlockActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			startActivity(new Intent(Ubicacion.this, LocalesActivity.class)
+			startActivity(new Intent(UbicacionActivity.this, LocalesActivity.class)
 			.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
 			break;
 		case R.id.menu_login:
-			startActivity(new Intent(Ubicacion.this, MiPerfilActivity.class)
+			startActivity(new Intent(UbicacionActivity.this, MiPerfilActivity.class)
 			.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT));
 			break;
 		}
