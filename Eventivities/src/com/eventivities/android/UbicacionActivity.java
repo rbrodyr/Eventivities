e09b9e342d0ab3c12e8f2db6556e2a5d4d62184e
jorpaspr <jorpaspr@gmail.com>
2013-01-08 09:43:37 +0100

    Añadido en UbicacionActivity el código para cargar de las preferencias el login.

--- Eventivities/src/com/eventivities/android/UbicacionActivity.java
+++ Eventivities/src/com/eventivities/android/UbicacionActivity.java
@@ -1,6 +1,8 @@
 package com.eventivities.android;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -46,6 +48,12 @@ public class UbicacionActivity extends SherlockActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater menuInflater = getSupportMenuInflater();
 		menuInflater.inflate(R.menu.general, menu);
+		SharedPreferences prefs = getSharedPreferences("LogInPreferences", Context.MODE_PRIVATE);
+		boolean login = prefs.getBoolean("logIn", false);
+		if(login)
+			menu.findItem(R.id.menu_login).setTitle(prefs.getString("usuarioActual", getString(R.string.menu_login).toUpperCase()));
+		else 
+			menu.findItem(R.id.menu_login).setTitle(getString(R.string.menu_login));
 		return true;
 	}
 
