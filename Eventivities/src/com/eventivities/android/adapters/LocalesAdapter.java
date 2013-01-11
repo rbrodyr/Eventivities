8dc4ae9830438b1aadc4b1d594d006eb8693692d
jorpaspr <jorpaspr@gmail.com>
2013-01-11 01:22:12 +0100

    Implementación parcial de la carga de imágenes (sin control de versiones) en las actividades Locales, Eventos y DetalleEvento.

--- Eventivities/src/com/eventivities/android/adapters/LocalesAdapter.java
+++ Eventivities/src/com/eventivities/android/adapters/LocalesAdapter.java
@@ -3,14 +3,18 @@ package com.eventivities.android.adapters;
 import java.util.List;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.eventivities.android.R;
 import com.eventivities.android.domain.Local;
+import com.eventivities.android.util.ImageAsyncHelper;
+import com.eventivities.android.util.ImageAsyncHelper.ImageAsyncHelperCallBack;
 
 public class LocalesAdapter extends ArrayAdapter<Local> {
 	
@@ -36,6 +40,24 @@ public class LocalesAdapter extends ArrayAdapter<Local> {
 			TextView textViewNombre = (TextView)view.findViewById(R.id.textViewNombreLocal);
 			if (textViewNombre != null)
 				textViewNombre.setText(local.getNombreLocal());
+			
+			final ImageView imageViewLocal = (ImageView)view.findViewById(R.id.imageViewLocal);
+			
+			if (imageViewLocal != null) {
+				ImageAsyncHelper imageAsyncHelper = new ImageAsyncHelper();
+				
+				Bitmap img = imageAsyncHelper.getBitmap(local.getNombreImg(),
+						new ImageAsyncHelperCallBack() {
+					
+					@Override
+					public void onImageSyn(Bitmap img) {
+						imageViewLocal.setImageBitmap(img);
+					}
+				}, null);
+				
+				if (img != null)
+					imageViewLocal.setImageBitmap(img);
+			}
 		}
 		
 		return view;
